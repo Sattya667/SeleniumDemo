e4d59cab72c6167e103a6d3f074f532aa3d9ea27
sibu.667@gmail.com <asnDomohani@01>
2020-08-22 10:23:52 +0530

    Updating java file

--- src/main/java/com/test/GoogleTest.java
+++ src/main/java/com/test/GoogleTest.java
@@ -16,9 +16,10 @@ public class GoogleTest {
 	
 	//1 //4 //7
 	
+	@BeforeMethod
 	public void setUp(){
 		System.out.println("Inside setup beforemethod");
-		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\Sibu\\Desktop\\Selenium\\chromedriver_win32\\chromedriver.exe");	
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Sibu\\Desktop\\Selenium\\chromedriver_win32\\chromedriver");	
 		driver = new ChromeDriver(); //launch chrome
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
@@ -31,54 +32,41 @@ public class GoogleTest {
 	//2
 	@Test(priority=1,groups="Title")
 	public void googleTitleTest(){
-		setUp();
 		String title = driver.getTitle();
 		System.out.println(title);
-		tearDown();
 		
 	}
 	
 	//8
 	@Test(priority=3,groups="Logo")
 	public void googleLogoTest(){
-		setUp();
 		boolean b = driver.findElement(By.xpath("//*[@id='hplogo']")).isDisplayed();
-		tearDown();
 	}
 
 	//5
 	@Test(priority=2,groups="Link Test")
 	public void mailLinkTest(){
-		setUp();
-		boolean b = driver.findElement(By.linkText("Gmail")).isDisplayed();
-		tearDown();
+		boolean b = driver.findElement(By.linkText("GMail")).isDisplayed();
 	}
 	
 	@Test(priority=4,groups="Test")
 	public void test1(){
-		setUp();
 		System.out.println("test1");
-		tearDown();
 	}
 	
 	@Test(priority=5,groups="Test")
 	public void test2(){
-		setUp();
 		System.out.println("test1");
-		tearDown();
 	}
 	
 	@Test(priority=6,groups="Test")
 	public void test3(){
-		setUp();
 		System.out.println("test1");
-		
-		tearDown();
 	}
 	
 	
 	//3 //6 //9
-	
+	@AfterMethod
 	public void tearDown(){
 		driver.quit();
 	}
